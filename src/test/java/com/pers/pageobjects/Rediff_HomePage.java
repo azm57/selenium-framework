e9e11815914b62f6746164f3ad8e896a5c59c4f0
azm57 <mdazeem5790@gmail.com>
2022-08-24 15:54:08 +0530

    enhance

--- src/test/java/com/pers/pageobjects/Rediff_HomePage.java
+++ src/test/java/com/pers/pageobjects/Rediff_HomePage.java
@@ -4,7 +4,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
